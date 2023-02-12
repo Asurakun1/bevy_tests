7b91b8449f9e58254714a4586ba8fbbf7e084ada
Asurakun1 <udon3699@gmail.com>
2023-02-12 00:42:50 -0800

    2d shape meshes

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,18 @@
-use bevy::{
-    prelude::*,
-    text::{self, Text2dBounds},
-};
-fn main() {
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+fn main(){
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(animate_translation)
-        .add_system(animate_rotation)
         .run();
 }
 
-#[derive(Component)]
-struct AnimateTranslation;
-#[derive(Component)]
-struct AnimateRotate;
-#[derive(Component)]
-struct AnimateScale;
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-    let text_style = TextStyle {
-        font,
-        font_size: 60.0,
-        color: Color::WHITE,
-    };
-
-    let text_alignment = TextAlignment::CENTER;
-
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>){
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("translation", text_style.clone())
-                .with_alignment(text_alignment),
-            ..default()
-        },
-        AnimateTranslation,
-    ));
 
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("Rotation", text_style.clone()).with_alignment(text_alignment),
-            ..default()
-        },
-        AnimateRotate,
-    ));
-}
-
-fn animate_translation(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, (With<Text>, With<AnimateTranslation>)>,
-) {
-    for mut transform in &mut query {
-        transform.translation.x = 100.0 * time.elapsed_seconds().sin() - 400.0;
-        transform.translation.y = 100.0 * time.elapsed_seconds().cos();
-    }
-}
-
-fn animate_rotation(
-    time: Res<Time>,
-    mut query: Query<&mut Transform, (With<Text>, With<AnimateRotate>)>,
-) {
-    for mut transform in &mut query {
-        transform.rotation = Quat::from_rotation_z(time.elapsed_seconds().cos());
-    }
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.0)),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        ..default()
+    });
 }
